f350824dd66864bc3ee3f4805ae166976fd48ca6
Andres Sebastian Almeida Yanez <andres.almeida.yanez@udla.edu.ec>
2024-10-28 08:01:36 -0500

    3. Control de variables para el ingreso de datos

--- AlmeidaSebastian_ExamenProgreso1/Models/Almeida.cs
+++ AlmeidaSebastian_ExamenProgreso1/Models/Almeida.cs
@@ -1,15 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AlmeidaSebastian_ExamenProgreso1.Models
 {
     public class Almeida{
+        [Key]
         public string Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Nombre { get; set; }
-        public float Decimal { get; set; }
+        [Range (0, 200, ErrorMessage = "Ingrese el peso en KG")]
+        public float Peso { get; set; }
+        [Required]
         public bool DonanteOrganos {  get; set; }
         public DateTime Fecha { get; set; }
         [ForeignKey("Celular")]
-        public Celular celular { get; set; }
+        [Display(Name = "Número celular")]
         public int IdCelular { get; set; }
+        public Celular celular { get; set; }
     }
 }
--- AlmeidaSebastian_ExamenProgreso1/Models/Celular.cs
+++ AlmeidaSebastian_ExamenProgreso1/Models/Celular.cs
@@ -5,9 +5,13 @@ namespace AlmeidaSebastian_ExamenProgreso1.Models
     public class Celular{
         [Key]
         public int Id { get; set; }
+        [Required]
         public int Numero { get; set; }
+        [MaxLength(100)]
         public string Modelo { get; set; }
+        [Range (1950, 2025, ErrorMessage = "Ingrese una fecha válida")]
         public int Anio { get; set; }
+        [Range (0, 10000, ErrorMessage = "Ingrese un precio válido")]
         public float Precio { get; set; }
     }
 }
